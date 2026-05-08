bf21bdf5652d03ade7ba94f45336226ec2500eb7
Agions <1051736049@qq.com>
2026-05-08 16:51:32 +0800

    clean: remove unused SceneConverter instance in PipelineIntegrator (-1L)

--- app/services/video/pipeline_integrator.py
+++ app/services/video/pipeline_integrator.py
@@ -74,7 +74,6 @@ class PipelineIntegrator(MonologueMaker):
 
         self.perspective_mapper = PerspectiveMapper(config=perspective_config)
         self.video_interleaver = VideoInterleaver(config=interleaver_config)
-        self.scene_converter = SceneConverter()
         self.emotion_generator = EmotionCurveGenerator()
 
         # 缓存最新穿插结果
